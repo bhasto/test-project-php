1c07b05d040e5a1391c8b0aa51acbb5be496c189
Branislav Hašto <bhasto@gmail.com>
2014-09-15 14:05:32 +0200

    Added backend validation.

--- create.php
+++ create.php
@@ -2,14 +2,52 @@
 
 $app = require "./core/app.php";
 
-// Create new instance of user
-$user = new User($app->db);
-// Insert it to database with POST data
-$user->insert(array(
-	'name' => $_POST['name'],
-	'email' => $_POST['email'],
-	'city' => $_POST['city']
-));
+function isValidEmail($s) {
+  return filter_var($s, FILTER_VALIDATE_EMAIL) !== FALSE;
+}
 
-// Redirect back to index
-header('Location: index.php');
+function isEmptyString($s) {
+  return strlen(trim($s)) == 0;
+}
+
+function isValidName($s) {
+  return !isEmptyString($s);
+}
+
+function isValidCity($s) {
+  return !isEmptyString($s);
+}
+
+function validateUser($data) {
+  $errorCodes = array();
+  if (!isValidName($data['name'])) $errorCodes[] = 'name';
+  if (!isValidEmail($data['email'])) $errorCodes[] = 'email';
+  if (!isValidCity($data['city'])) $errorCodes[] = 'city';
+
+  $valid = empty($errorCodes);
+  $result = array('valid' => $valid);
+  if (!$valid) $result['errorCodes'] = $errorCodes;
+  return $result;
+}
+
+
+$data = array(
+  'name' => isset($_POST['name']) ? $_POST['name'] : NULL,
+  'email' => isset($_POST['email']) ? $_POST['email'] : NULL,
+  'city' => isset($_POST['city']) ? $_POST['city'] : NULL,
+);
+
+$validationResult = validateUser($data);
+
+if ($validationResult['valid']) {
+  $user = new User($app->db);
+  $user->insert($data);
+  header('Location: index.php');
+} else {
+  $users = User::find($app->db, '*');
+  $app->renderView('index', array(
+	  'users' => $users,
+    'errorCodes' => $validationResult['errorCodes'],
+    'data' => $data
+  ));
+}
--- views/index.php
+++ views/index.php
@@ -24,30 +24,41 @@
 <h2>Add user</h2>
 <form method="post" action="create.php" class="form-horizontal">
 
-  <div class="form-group">
- <label for="name" class="col-sm-1 control-label">Name:</label>
+  <?php if (isset($errorCodes)){ ?>
+  <?php $errorCodeToMessage = array('name' => 'Invalid name', 'email' => 'Invalid e-mail', 'city' => 'Invalid city'); ?>
+    <div class="alert alert-danger">
+      <ul>
+        <?php foreach($errorCodes as $code){?>
+          <li><?=$errorCodeToMessage[$code]?></li>
+        <?php }?>
+      </ul>
+    </div>
+  <?php }?>
+
+  <div class="form-group required">
+    <label for="name" class="col-sm-1 control-label">Name:</label>
     <div class="col-sm-11">
-   <input name="name" input="text" id="name" class="form-control"/>
+      <input name="name" input="text" id="name" class="form-control" <?php if (isset($data['name'])) {?> value="<?=$data['name']?>" <?php } ?>/>
     </div>
   </div>
 
-  <div class="form-group">
- <label for="email" class="col-sm-1 control-label">E&#8209;mail:</label>
+  <div class="form-group required">
+    <label for="email" class="col-sm-1 control-label">E&#8209;mail:</label>
     <div class="col-sm-11">
-      <input name="email" input="text" id="email" class="form-control"/>
+      <input name="email" input="text" id="email" class="form-control" <?php if (isset($data['email'])) {?> value="<?=$data['email']?>" <?php } ?>/>
     </div>
   </div>
 
-  <div class="form-group">
-  <label for="city" class="col-sm-1 control-label">City:</label>
+  <div class="form-group required">
+    <label for="city" class="col-sm-1 control-label">City:</label>
     <div class="col-sm-11">
-   <input name="city" input="text" id="city" class="form-control"/>
+      <input name="city" input="text" id="city" class="form-control" <?php if (isset($data['city'])) {?> value="<?=$data['city']?>" <?php } ?>/>
     </div>
   </div>
 
   <div class="form-group">
     <div class="col-sm-offset-1 col-sm-11">
-   <button class="btn btn-primary btn-lg">Create new row</button>
+      <button class="btn btn-primary btn-lg">Create new row</button>
     </div>
   </div>
 </form>
